32742afa90a5434d4f08002fc9be4b2fe0715ec9
Zi <zihaoliang0413@gmail.com>
2025-11-26 22:46:28 -0800

    reverting to before dev mode

--- src/landing/mod.rs
+++ src/landing/mod.rs
@@ -50,7 +50,7 @@ impl eframe::App for LandingPage {
                 ui.text_edit_singleline(&mut self.label);
             });
             ui.horizontal(|ui| {
-                ui.label("Write something else: ");
+                ui.label("Write something new: ");
                 ui.text_edit_singleline(&mut self.label);
             });
 
@@ -73,59 +73,6 @@ impl eframe::App for LandingPage {
         });
     }
 }
-
-// impl crate::page::Page for LandingPage {
-//     fn update(&mut self, ctx: &egui::Context) {
-//         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-//         // For inspiration and more examples, go to https://emilk.github.io/egui
-
-//         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-//             // The top panel is often a good place for a menu bar:
-
-//             egui::MenuBar::new().ui(ui, |ui| {
-//                 // NOTE: no File->Quit on web pages!
-//                 let is_web = cfg!(target_arch = "wasm32");
-//                 if !is_web {
-//                     ui.menu_button("File", |ui| {
-//                         if ui.button("Quit").clicked() {
-//                             ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-//                         }
-//                     });
-//                     ui.add_space(16.0);
-//                 }
-
-//                 egui::widgets::global_theme_preference_buttons(ui);
-//             });
-//         });
-
-//         egui::CentralPanel::default().show(ctx, |ui| {
-//             // The central panel the region left after adding TopPanel's and SidePanel's
-//             ui.heading("eframe template");
-
-//             ui.horizontal(|ui| {
-//                 ui.label("Write something: ");
-//                 ui.text_edit_singleline(&mut self.label);
-//             });
-
-//             ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-//             if ui.button("Increment").clicked() {
-//                 self.value += 1.0;
-//             }
-
-//             ui.separator();
-
-//             ui.add(egui::github_link_file!(
-//                 "https://github.com/emilk/eframe_template/blob/main/",
-//                 "Source code."
-//             ));
-
-//             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-//                 powered_by_egui_and_eframe(ui);
-//                 egui::warn_if_debug_build(ui);
-//             });
-//         });
-//     }
-// }
 
 fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
     ui.horizontal(|ui| {
